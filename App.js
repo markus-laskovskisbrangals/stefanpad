9d8c1116331d4d3707d98037ee351eead00eaf9c
markus-laskovskisbrangals <72466773+markus-laskovskisbrangals@users.noreply.github.com>
2020-12-02 11:27:09 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './app/screens/Welcome';
+import Main from './app/screens/Main';
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
+
